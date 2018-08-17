ad2d82f9b0c2b22ff93075b0560327aa7a2261fa
Ky Duong <pn.kyduong95@gmail.com>
2018-08-17 11:22:44 +0700

    - first commit

---
+++ HomeTestTiki/HomeTestTiki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeTestTiki
+//
+//  Created by Ky Duong on 8/17/18.
+//  Copyright © 2018 Ky Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
